139baeb0b74f8c5d5d3573536a89a1057a72c9a8
Giuseppe <Giuseppe@mbp-di-giuseppe.homenet.telecomitalia.it>
2015-01-14 19:00:34 +0100

    modifiche

---
+++ storescripts/create_tabella_indirizzo.php
@@ -0,0 +1,28 @@
+<?php
+
+require 'connect_to_mysql.php';
+
+$sqlCommand = "CREATE TABLE indirizzo (
+   
+    cod int(8) auto_increment NOT NULL,
+    id int(8) NOT NULL,
+    via varchar(24) NOT NULL,
+    numerociv varchar(24) NOT NULL,
+    cap varchar(24) NOT NULL,
+    citta varchar(24) NOT NULL,
+    provincia varchar(24) NOT NULL,
+    nazione varchar(24) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY id REFERENCES utente(id)
+    ) ";
+
+if (mysql_query($sqlCommand)) 
+{
+    echo ("tabella UTENTE creata correttamente");
+}
+else
+{
+    echo ("ERRORE FATALE, tabella UTENTE non creata");
+}
+
+?>   
