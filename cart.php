6d394092ba8262b1dd14adbabbe5acbf19c2f375
tod <tod@tod-PC>
2015-01-23 23:26:10 +0100

    Merge origin/master
    Conflicts:
    	storescripts/crea_tabella_metodo_pagamento.php

--- cart.php
+++ cart.php
@@ -15,6 +15,7 @@ if (isset($_GET["n"])) {
     $sql = mysql_query("DELETE FROM carrello WHERE cart_element='$item_to_delete' LIMIT 1") or die(mysql_error());
 }
 ?>
+
 <body>
     <div align="center" id="mainWrapper">
         <?php include_once("./template_header.php"); ?>
@@ -38,15 +39,19 @@ if (isset($_GET["n"])) {
                         $prod_price = $row2["price"];
                         $tot += $prod_quantity * $prod_price;
                     }
-                    $string = '<td><img src="inventory_images/'.$row["prod_code"].'.jpg" '.'height='.'"20px"></td><td id='.'"carrello_td">' . $prod_name . "</td><td id="."carrello_td".">" . $prod_price . "€</td><td id="."carrello_td".">" . $prod_quantity . "</td><td id="."carrello_td".">" . $prod_price * $prod_quantity . "€</td>";
-                    echo $string.'<td> <a href="cart.php?n=' . $prod_cartnumber . ' ">cancella</a><br /> </td>';
+                    $string = '<td><img src="inventory_images/' . $row["prod_code"] . '.jpg" ' . 'height=' . '"20px"></td><td id=' . '"carrello_td">' . $prod_name . "</td><td id=" . "carrello_td" . ">" . $prod_price . "€</td><td id=" . "carrello_td" . ">" . $prod_quantity . "</td><td id=" . "carrello_td" . ">" . $prod_price * $prod_quantity . "€</td>";
+                    echo $string . '<td> <a href="cart.php?n=' . $prod_cartnumber . ' ">cancella</a><br /> </td>';
                     echo "</tr>";
                 }
                 ?>
                 <tr>
+                
                     <td></td>  <td></td> <td></td>  <td id="totale" colspan="2" style="text-align: right;"><?php echo "Totale: $tot €" ?></td>
-                    <td></td>  <td></td> <td></td> <td></td> <td><input type="submit" name="pay" id="button" value="Completa l'ordine e paga ora" />
+
+                    <td></td>  <td></td> <td></td> <td></td> <td>
+                        <input type="submit" name="pay" id="button" value="Completa l'ordine e paga ora" />
                     </td>
+                
                 </tr> 
 
             </table>
