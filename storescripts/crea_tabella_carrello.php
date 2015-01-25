30ed91005a448e308e04cc5ac7e3f92aef67f898
tod <tod@tod-PC>
2015-01-25 18:37:44 +0100

    Merge origin/master
    Conflicts:
    	storescripts/crea_tabella_metodo_pagamento.php

--- storescripts/crea_tabella_carrello.php
+++ storescripts/crea_tabella_carrello.php
@@ -6,7 +6,7 @@ $sqlCommand = "CREATE TABLE IF NOT EXISTS carrello (
     cart_element int(4) NOT NULL auto_increment,
     prod_code int(4) NOT NULL,
     user_id int(32) NOT NULL,
-    quantity int (16) NOT NULL,
+    quantity int (8) NOT NULL,
     PRIMARY KEY (cart_element, user_id),
     FOREIGN KEY (prod_code) REFERENCES prodotto(prod_code),
     FOREIGN KEY(user_id) REFERENCES utente(id)
